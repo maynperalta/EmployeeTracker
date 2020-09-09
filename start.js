a81dfb8085990526628ec21499e82207df8af79a
Maynard Peralta <maynperalta@gmail.com>
2020-09-09 17:31:03 -0600

    first commit

---
+++ start.js
@@ -0,0 +1,29 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Ir0n_S41n+",
+  database: "employee_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  console.log("employee_db connected")
+});
+
+connection.query("SELECT * FROM employee; ", function(err, data) {
+    if(err) throw err;
+    console.log(data)
+})
