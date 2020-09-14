144d11b7d91c2f14da492888d53201a2a674672b
Maynard Peralta <maynperalta@gmail.com>
2020-09-13 19:42:37 -0600

    fixed table display for employees

--- start.js
+++ start.js
@@ -93,7 +93,7 @@ function start() {
 }
 
 function viewEmployees() {
-  connection.query("SELECT employee.first_name, employee.last_name, employee.role_id FROM employee INNER JOIN role ON (role.id = employee.role_id)", function(err, res) {
+  connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employee LEFT JOIN employee manager on manager.id = employee.manager_id INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id)", function(err, res) {
     if(err) throw err;
     console.log("Employees:")
     console.table(res)
